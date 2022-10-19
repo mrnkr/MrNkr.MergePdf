84b6ea55f96933c06adf2234ddf370e0602f6bbb
Alvaro Nicoli <xmr.nkr@gmail.com>
2022-10-19 20:23:07 -0300

    first commit

---
+++ MrNkr.MergePdf.Core/MergedPdfBuilder.cs
@@ -0,0 +1,42 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+
+namespace MrNkr.MergePdf.Core;
+
+public class MergedPdfBuilder
+{
+    private readonly IList<string> _files = new List<string>();
+    private string? _dest;
+
+    public MergedPdfBuilder AddFile(string file)
+    {
+        _files.Add(file);
+        return this;
+    }
+
+    public MergedPdfBuilder SaveAs(string path)
+    {
+        _dest = path;
+        return this;
+    }
+
+    public void Build()
+    {
+        if (_dest is null)
+        {
+            throw new InvalidOperationException($"Call {nameof(SaveAs)} before calling {nameof(Build)}");
+        }
+
+        PdfDocument pdf = new(new PdfWriter(_dest));
+        PdfMerger merger = new(pdf);
+
+        foreach (string file in _files)
+        {
+            PdfDocument sourcePdf = new(new PdfReader(file));
+            merger.Merge(sourcePdf, 1, sourcePdf.GetNumberOfPages());
+            sourcePdf.Close();
+        }
+
+        pdf.Close();
+    }
+}
---
+++ MrNkr.MergePdf/Program.cs
@@ -0,0 +1,59 @@
+﻿using MrNkr.MergePdf.Core;
+
+Console.WriteLine("Merge some PDFs");
+
+MergedPdfBuilder builder = new();
+
+while (true)
+{
+    Console.Write("Insert the path to a pdf file >> ");
+    string? path = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(path))
+    {
+        break;
+    }
+
+    if (!File.Exists(path))
+    {
+        Console.WriteLine("File does not exist, try again");
+        continue;
+    }
+
+    if (!path.EndsWith(".pdf"))
+    {
+        Console.WriteLine("File does not seem to have the right extension, try again");
+        continue;
+    }
+
+    builder.AddFile(path);
+}
+
+while (true)
+{
+    Console.Write("Insert the path to the output file >> ");
+    string? path = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(path))
+    {
+        Console.WriteLine("Please define where the output should be saved");
+        continue;
+    }
+
+    if (File.Exists(path))
+    {
+        Console.WriteLine("File already exists, try again");
+        continue;
+    }
+
+    if (!path.EndsWith(".pdf"))
+    {
+        Console.WriteLine("File does not seem to have the right extension, try again");
+        continue;
+    }
+
+    builder.SaveAs(path);
+    break;
+}
+
+builder.Build();
